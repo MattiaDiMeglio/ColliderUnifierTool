24a29812cc1eaa7f533c7915e96590349c62a671
MattiaDiMeglio <mat.dimeglio@gmail.com>
2023-03-03 14:56:06 +0100

    Update BoxColliderUnifier.cs

--- BoxColliderUnifier.cs
+++ BoxColliderUnifier.cs
@@ -16,13 +16,16 @@ public class BoxColliderUnifier : MonoBehaviour
     private void DoUnify()
     {
         BoxCollider collider = gameObject.AddComponent<BoxCollider>();//we add a new collider
+        BoxCollider childCollider = null;
         Bounds newBounds = gameObject.transform.GetChild(0).GetComponent<Renderer>().bounds;//we get the first child bounds
         gameObject.transform.GetChild(0).GetComponent<BoxCollider>().enabled = false;
+        //gameObject.transform.GetChild(0).GetComponent<BoxCollider>().enabled = false;
         for (int i = 1; i < gameObject.transform.childCount; i++)//encapsulates every other child bounds
         {
             newBounds.Encapsulate(gameObject.transform.GetChild(i).GetComponent<Renderer>().bounds);
-            gameObject.transform.GetChild(i).GetComponent<BoxCollider>().enabled = false;
-
+            childCollider = gameObject.transform.GetChild(i).GetComponent<BoxCollider>();
+            if (childCollider != null)
+                childCollider.enabled = false;
         }
          //we divide the size of the bounds by the parent scale
         collider.size = new Vector3(newBounds.size.x/gameObject.transform.localScale.x, 
